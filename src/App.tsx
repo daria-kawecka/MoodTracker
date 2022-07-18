f8a2ca0a864dded5d08f7cfeb0d261c3ca8a6198
daria-kawecka <daria.kawecka28@gmail.com>
2022-07-18 13:21:50 +0200

    Create home screen with MoodPicker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React, { FC } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { BottomTabsNavigator } from './screens/BottomTabs.navigator';
 
 export const App: FC = () => {
-  return <View style={styles.container} />;
+  return (
+    <NavigationContainer>
+      <BottomTabsNavigator />
+    </NavigationContainer>
+  );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'teal',
-  },
-});
---
+++ src/screens/Home.screen.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { MoodPicker } from '../components/MoodPicker';
+
+export const Home: FC = () => {
+  return (
+    <View style={styles.container}>
+      <MoodPicker />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
