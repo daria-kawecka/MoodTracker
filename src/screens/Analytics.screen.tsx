84bfcc997a4314eeb513fab2f419f425f1c0b884
daria-kawecka <daria.kawecka28@gmail.com>
2022-07-28 16:06:57 +0200

    Create chart

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,35 @@
+import { groupBy } from 'lodash';
 import React, { FC } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { VictoryPie } from 'victory-native';
+import { useAppContext } from '../App.provider';
+import { theme } from '../theme';
 
 export const Analytics: FC = () => {
+  const { moodList } = useAppContext();
+
+  const data = Object.entries(groupBy(moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
     <View>
-      <Text>Analytics</Text>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        data={data}
+      />
     </View>
   );
 };
